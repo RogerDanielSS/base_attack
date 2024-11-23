4a51a9acfc318fd8da47312a11051e2590088b17
rogerdanielss <danielsimoesp@outlook.com>
2024-11-23 15:47:00 -0300

    finish: implement raft consensus

--- consensus/data/Node.java
+++ consensus/data/Node.java
@@ -7,14 +7,25 @@ import java.util.*;
 import javafx.application.Platform;
 
 public class Node extends Thread {
-    private NodesThreadsController threadsController;
+    private final NodesThreadsController threadsController;
     private final int nodeId;
-    private ArrayList<Integer> numbers;
     private final int port;
     private final List<Integer> otherNodePorts;
-    private final Map<List<Integer>, Integer> receivedOrders = new HashMap<>(); // Contador de ordens recebidas
-    private volatile boolean consensusReached = false; // Indica se o nó alcançou consenso
+    private ArrayList<Integer> numbers;
     private ServerSocket serverSocket; // ServerSocket do nó
+
+    private int leaderPort = -1;
+    private boolean consensusReached = false; // Indica se o nó alcançou consenso
+    private boolean listening = true;
+
+    private String expectedLeaderPort = "";
+    private boolean expectingForLeaderResponse = false;
+    private boolean openVoting = false;
+    private int leaderCandidatationResponses = 0;
+    private int positiveLeaderCandidatationResponses = 0;
+
+    private int orderVotingResponses = 0;
+    private int positiveOrderVotingResponses = 0;
 
     public Node(NodesThreadsController threadsController, int nodeId, ArrayList<Integer> numbers, int port,
             List<Integer> otherNodePorts) {
@@ -29,110 +40,289 @@ public class Node extends Thread {
     public void run() {
         try {
             serverSocket = new ServerSocket(port);
-            // System.out.println("Node " + nodeId + " listening on port " + port);
+            // Thread to constantly listen for incoming proposals
+            new Thread(this::listen).start();
 
             // Inicia o processo de consenso até que o consenso seja alcançado
             while (!consensusReached) {
-                Platform.runLater(() -> {
-                    threadsController.setSoldierSubtitle(nodeId, numbers.toString(), "black");
-                });
+                try {
+                Thread.sleep(getRandomNumber());
+                } catch (Exception e) {
+                }
 
-                sendProposalToOtherNodes();
+                if (leaderPort == -1 && !openVoting)
+                    handleLackOfLeader();
 
-                // Limpa as ordens recebidas para uma nova rodada e aguarda propostas dos outros
-                // nós
-                receivedOrders.clear();
-                receiveProposalsFromOtherNodes();
+                if (this.leaderPort == this.port)
+                    handleOrderVoting();
 
-                // Decide consenso após receber todas as propostas
-                decideConsensus();
-
-                // Espera um pouco antes da próxima rodada (simulação de espera)
                 Thread.sleep(3000);
             }
 
-            Platform.runLater(() -> {
-                threadsController.setSoldierSubtitle(nodeId, numbers.toString(), "green");
-            });
+            System.out.println(nodeId + ": " + numbers);
 
-            if (nodeId == 0)
-                System.out.println("Node " + nodeId + " reached final consensus on order: " + numbers);
         } catch (IOException | InterruptedException e) {
-
-            if (nodeId == 0)
-                System.out.println("Node " + nodeId + " could not start: " + e.getMessage());
+            System.out.println("Node " + nodeId + " could not start: " + e.getMessage());
         }
     }
 
-    private void sendProposalToOtherNodes() {
+    public int getRandomNumber() {
+        Random random = new Random();
+        // Generate a random number between 0 and 4500, then add 500 to shift the range
+        return 500 + random.nextInt(4501);
+    }
+
+    private void handleLackOfLeader() {
+        positiveLeaderCandidatationResponses = 0;
+        leaderCandidatationResponses = 0;
+
+        HashMap<String, String> message = new HashMap<String, String>();
+
+        message.put("type", "candidatation");
+        message.put("content", "");
+        message.put("origin", "" + port);
+
         for (int otherPort : otherNodePorts) {
-            new Thread(() -> {
-                try (Socket socket = new Socket("localhost", otherPort);
-                        ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream())) {
-                    out.writeObject(numbers);
-                    Platform.runLater(() -> {
-                        threadsController.animateSendMessage(numbers.toString(), nodeId, otherPort - 5000);
-                    });
-
-                    if (nodeId == 0)
-                        System.out.println("Node " + nodeId + " sent proposal to port " + otherPort);
-                } catch (IOException e) {
-                    System.out.println(
-                            "Node " + nodeId + " could not connect to port " + otherPort + ": " + e.getMessage());
-                }
-            }).start();
+            sendMessage(message, otherPort);
         }
+
+        try {
+            while (leaderCandidatationResponses != 9 && positiveLeaderCandidatationResponses <= 4) {
+                Thread.sleep(500);
+            }
+        } catch (Exception e) {
+        }
+
+        defineLeadership();
     }
 
-    private void receiveProposalsFromOtherNodes() {
-        for (int i = 0; i < otherNodePorts.size(); i++) { // Recebe de cada nó
+    private void handleOrderVoting() {
+        orderVotingResponses = 0;
+        positiveOrderVotingResponses = 0;
+
+        HashMap<String, String> message = new HashMap<String, String>();
+
+        message.put("type", "order-voting");
+        message.put("content", numbers.toString().replaceAll("[^0-9]", ""));
+        message.put("origin", "" + port);
+
+        for (int otherPort : otherNodePorts) {
+            sendMessage(message, otherPort);
+        }
+
+        try {
+            while (orderVotingResponses != 9 && positiveOrderVotingResponses <= 6) {
+                System.out.println(nodeId + " orderVotingResponses: " + orderVotingResponses);
+                System.out.println(nodeId + " positiveOrderVotingResponses: " + positiveOrderVotingResponses);
+                Thread.sleep(500);
+            }
+        } catch (Exception e) {
+        }
+
+        if (positiveOrderVotingResponses > 6)
+            sendMyOrderToFollowers();
+        else
+            dropLeadership();
+
+    }
+
+    private void defineLeadership() {
+        System.out.println("candidate " + port + " has " + positiveLeaderCandidatationResponses + " votes");
+        HashMap<String, String> message = new HashMap<String, String>();
+
+        String content = "IM-NOT-THE-LEADER";
+
+        if (this.positiveLeaderCandidatationResponses > 4) {
+            leaderPort = port;
+            System.out.println(nodeId + " is leader");
+            content = "IM-THE-LEADER";
+        }
+
+        message.put("type", "leader-definition");
+        message.put("content", content);
+        message.put("origin", "" + port);
+
+        for (int otherPort : otherNodePorts)
+            sendMessage(message, otherPort);
+    }
+
+    private void dropLeadership() {
+        System.out.println(port + ": Order disapproved. Droping leadership");
+        HashMap<String, String> message = new HashMap<String, String>();
+
+        message.put("type", "leader-definition");
+        message.put("content", "DROP-LEADERSHIP");
+        message.put("origin", "" + port);
+
+        for (int otherPort : otherNodePorts)
+            sendMessage(message, otherPort);
+    }
+
+    private void sendMyOrderToFollowers() {
+        System.out.println(port + ": Order approved. Broadcasting");
+        HashMap<String, String> message = new HashMap<String, String>();
+
+        message.put("type", "order-definition");
+        message.put("content", numbers.toString().replaceAll("[^0-9]", ""));
+        message.put("origin", "" + port);
+
+        for (int otherPort : otherNodePorts) {
+            System.out.println(nodeId + " sent order to " + otherPort);
+            sendMessage(message, otherPort);
+        }
+        setReachedConsensus();
+    }
+
+    private void sendMessage(HashMap<String, String> message, int port) {
+        new Thread(() -> {
+            try (Socket socket = new Socket("localhost", port);
+                    ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream())) {
+
+                out.writeObject(message);
+            } catch (IOException e) {
+                System.out.println(
+                        "Node " + nodeId + " could not connect to port " + port + ": " +
+                                e.getMessage());
+            }
+        }).start();
+    }
+
+    private void listen() {
+        while (listening) {
             try (Socket socket = serverSocket.accept();
                     ObjectInputStream in = new ObjectInputStream(socket.getInputStream())) {
                 @SuppressWarnings("unchecked")
-                ArrayList<Integer> receivedOrder = (ArrayList<Integer>) in.readObject();
+                HashMap<String, String> receivedMessage = (HashMap<String, String>) in.readObject();
 
-                if (nodeId == 0)
-                    System.out.println("Node " + nodeId + " received order: " + receivedOrder);
-
-                // Registra e conta a ordem recebida
-                receivedOrders.merge(receivedOrder, 1, Integer::sum);
+                handleReceivedMessage(receivedMessage);
             } catch (IOException | ClassNotFoundException e) {
-
-                if (nodeId == 0)
-                    System.out.println("Node " + nodeId + " connection error: " + e.getMessage());
+                System.out.println("Node " + nodeId + " connection error: " +
+                        e.getMessage());
             }
         }
     }
 
-    private void decideConsensus() {
-        // Encontra a ordem mais popular
-        Map.Entry<List<Integer>, Integer> consensusOrder = receivedOrders.entrySet().stream()
-                .max(Map.Entry.comparingByValue())
-                .orElseThrow(() -> new RuntimeException("Consensus decision not found")); // Deve sempre retornar uma
-                                                                                          // entrada válida
+    private void handleReceivedMessage(HashMap<String, String> message) {
+        switch (message.get("type")) {
+            case "candidatation":
+                respondCandidate(message);
+                break;
+            case "candidatation-response":
+                storeCandidatationResponses(message);
+                break;
+            case "leader-definition":
+                verifyAndSetLeader(message);
+                break;
 
-        if (nodeId == 0)
-            System.out.println("\n\n" + receivedOrders);
-        // Atualiza o array para a ordem consensual se necessário
-        ArrayList<Integer> newOrder = new ArrayList<>(consensusOrder.getKey());
-        if (!newOrder.equals(numbers)) {
-
-            if (nodeId == 0)
-                System.out.println("Node " + nodeId + " changed order: " + newOrder);
-            numbers = newOrder;
-        } else if (checkReachedGeneralConsensus()) {
-            // Se o array já estiver em consenso, marca o nó como tendo alcançado consenso
-            consensusReached = true;
+            case "order-voting":
+                respondOrderVoting(message);
+                break;
+            case "order-voting-response":
+                storeOrderVoteResponses(message);
+                break;
+            case "order-definition":
+                setOrder(message);
+                break;
+            default:
+                break;
         }
     }
 
-    private boolean checkReachedGeneralConsensus() {
-        if (receivedOrders.size() == 1) {
-            // Get the only entry in the map
-            int count = receivedOrders.values().iterator().next();
-            // Check if the count is 9 or greater
-            return count >= 9;
+    private void setOrder(HashMap<String, String> message) {
+        this.numbers = charArrayToIntArrayList(message.get("content").toCharArray());
+        setReachedConsensus();
+        System.out.println("Node " + nodeId + "changed order to: " + message.get("content"));
+    }
+
+    private void setReachedConsensus() {
+        this.consensusReached = true;
+        this.listening = false;
+    }
+
+    private void verifyAndSetLeader(HashMap<String, String> message) {
+        if (message.get("content").equals("IM-THE-LEADER")) {
+
+            System.out.println("Node " + nodeId + " defined port " + message.get("origin") + " as its leader");
+            leaderPort = Integer.parseInt(message.get("origin"));
+            expectingForLeaderResponse = false;
+            expectedLeaderPort = "";
+
+        } else if (message.get("content").equals("IM-NOT-THE-LEADER")
+                && expectedLeaderPort.equals(message.get("origin"))) {
+
+            expectingForLeaderResponse = false;
+            expectedLeaderPort = "";
+
+        } else if (message.get("content").equals("DROP-LEADERSHIP")
+                && ("" + leaderPort).equals(message.get("origin"))) {
+            leaderPort = -1;
         }
-        return false;
+
+        openVoting = false;
+    }
+
+    private void respondOrderVoting(HashMap<String, String> message) {
+        HashMap<String, String> responseMessage = new HashMap<String, String>();
+
+        responseMessage.put("type", "order-voting-response");
+        responseMessage.put("content", "" + getRandomBoolean());
+        responseMessage.put("origin", "" + nodeId);
+
+        sendMessage(responseMessage, Integer.parseInt(message.get("origin")));
+    }
+
+    private void respondCandidate(HashMap<String, String> message) {
+        openVoting = true;
+
+        HashMap<String, String> responseMessage = new HashMap<String, String>();
+
+        boolean content = false;
+
+        if (!expectingForLeaderResponse) {
+            content = getRandomBoolean();
+        }
+
+        if (content) {
+            expectedLeaderPort = message.get("origin");
+            this.expectingForLeaderResponse = true;
+        }
+
+        responseMessage.put("type", "candidatation-response");
+        responseMessage.put("content", "" + content);
+        responseMessage.put("origin", "" + nodeId);
+
+        sendMessage(responseMessage, Integer.parseInt(message.get("origin")));
+    }
+
+    private void storeCandidatationResponses(HashMap<String, String> responseMessage) {
+        leaderCandidatationResponses += 1;
+        if (responseMessage.get("content").equals("true"))
+            positiveLeaderCandidatationResponses += 1;
+
+    }
+
+    private void storeOrderVoteResponses(HashMap<String, String> responseMessage) {
+        orderVotingResponses += 1;
+        if (responseMessage.get("content").equals("true"))
+            positiveOrderVotingResponses += 1;
+
+    }
+
+    private static boolean getRandomBoolean() {
+        Random random = new Random();
+        return random.nextBoolean();
+    }
+
+    private static ArrayList<Integer> charArrayToIntArrayList(char[] charArray) {
+        ArrayList<Integer> intList = new ArrayList<>();
+
+        for (char c : charArray) {
+            // Convert char to int and add to the list
+            if (Character.isDigit(c)) {
+                intList.add(Character.getNumericValue(c));
+            }
+        }
+
+        return intList;
     }
 }
